a628e3d6a6fa878a6dc116c6729cb3952060a986
phulv <vinhphuluong94@gmail.com>
2025-09-29 22:56:57 +0700

    Initial Commit

---
+++ ZaloNewsFeed/ZaloNewsFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZaloNewsFeedApp.swift
+//  ZaloNewsFeed
+//
+//  Created by Nguyen Nhat Anh on 29/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ZaloNewsFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
